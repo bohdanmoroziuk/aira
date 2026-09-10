b87ba747cc9608278949571a8115aa7303bf95ce
Bohdan Moroziuk <bogdan7morozyuk@gmail.com>
2026-09-10 13:34:01 +0300

    feat: add landing page linking to the chat

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <h1>Aira</h1>
-  </div>
+  <UContainer class="flex flex-col items-center justify-center gap-4 h-screen">
+    <h1 class="text-[2.25rem] font-bold">Aira</h1>
+
+    <UButton to="/chat"> Start a new chat </UButton>
+  </UContainer>
 </template>
