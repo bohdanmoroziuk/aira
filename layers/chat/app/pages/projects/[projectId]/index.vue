44f49285ca9c3b5427d3766741c5e6d9e141b325
Bohdan Moroziuk <bogdan7morozyuk@gmail.com>
2026-09-21 11:01:09 +0300

    fix: stop cropping the card border on the project chat list

--- layers/chat/app/pages/projects/[projectId]/index.vue
+++ layers/chat/app/pages/projects/[projectId]/index.vue
@@ -8,7 +8,7 @@ const chats = getProjectChats(projectId)
 </script>
 
 <template>
-  <div>
+  <div class="p-px">
     <div
       v-if="chats?.length"
       class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"
